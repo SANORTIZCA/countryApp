33243bc8dc2b818bf98df24ab8ec090d20bb2626
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-05-19 19:50:32 -0500

    Se corrige una importación de más en app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
