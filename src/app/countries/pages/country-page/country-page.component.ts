57dbaef45d6aa61c5d34abdbd50de9e4575cddf2
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-05-21 10:13:29 -0500

    Se agregan la funcionalidad a countryPage en donde se inyecta el servicio de ActivatedRoute y CountriesService además, se usa un observable (activatedRoute) dentro de otro observable (countriesServices) y así la respuesta de subscribsción de un observable es la respuesta del otro observable.

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  /*
+  observable: recomendación de usar
+  activedRoute:
+   */
+  private _activatedRoute: ActivatedRoute;
+  private _countriesService: CountriesService
+
+  constructor(activatedRoute: ActivatedRoute, countriesService: CountriesService){
+    this._activatedRoute = activatedRoute;
+    this._countriesService = countriesService;
+  }
+
+  /* Observable dentro de un observable porque para realizar uno se necesita la respuesta del anterior */
+  public ngOnInit(): void {
+    this._activatedRoute.params
+    .subscribe(({id}) => {
+      this._countriesService.searchCountryByAlphaCode(id)
+        .subscribe(country => {
+          console.log({country});
+        });
+    });
+  }
+
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, ObservedValueOf, catchError, of } from 'rxjs';
 
 import { Country } from '../interfaces/country.interface';
 
@@ -8,7 +8,6 @@ import { Country } from '../interfaces/country.interface';
   providedIn: 'root',
 })
 export class CountriesService {
-
   private _httpClient: HttpClient;
   private _apiUrl: string = 'https://restcountries.com/v3.1';
 
@@ -16,31 +15,32 @@ export class CountriesService {
     this._httpClient = httpClient;
   }
 
-  public searchCapital( term: string): Observable<Country[]>{
+  public searchCountryByAlphaCode(code: string): Observable<Country[]> {
+    const url: string = `${this._apiUrl}/alpha/${code}`;
+    return this._httpClient.get<Country[]>(url)
+      .pipe(
+        catchError(() => of([]))
+      );
+  }
+
+  public searchCapital(term: string): Observable<Country[]> {
     /* Observable, y se realiza la petición http, en donde el get se le dice que retorna y al observable también*/
     /* Además hay que subscriberse a la respuesta, por que si no se susbcribe, se define pero no se ejecuta la petición*/
     const url: string = `${this._apiUrl}/capital/${term}`;
     /* el método pipe de los observables que sirve para especificar diferentes operadores de rxjs, que son que con un flujo de datos podemos hacer lo que queramos con ese flujo. Entre estos operadores tenemos map(), tap()*/
-    return this._httpClient.get<Country[]>(url)
-      .pipe(
-        /* EL of sirve para construir rapidamente un obvservable dependiendo del argumento enviado, en este caso queremos que dependiendo de si sucede el error que regrese un nuevo observable que va a ser un array vacio */
-        catchError(() => of([]))
-      );
+    return this._httpClient.get<Country[]>(url).pipe(
+      /* EL of sirve para construir rapidamente un obvservable dependiendo del argumento enviado, en este caso queremos que dependiendo de si sucede el error que regrese un nuevo observable que va a ser un array vacio */
+      catchError(() => of([]))
+    );
   }
 
-  public searchCountry( term: string): Observable<Country[]>{
+  public searchCountry(term: string): Observable<Country[]> {
     const url: string = `${this._apiUrl}/name/${term}`;
-    return this._httpClient.get<Country[]>(url)
-      .pipe(
-        catchError(() => of([]))
-      );
+    return this._httpClient.get<Country[]>(url).pipe(catchError(() => of([])));
   }
 
-  public searchRegion( region: string): Observable<Country[]>{
+  public searchRegion(region: string): Observable<Country[]> {
     const url: string = `${this._apiUrl}/region/${region}`;
-    return this._httpClient.get<Country[]>(url)
-      .pipe(
-        catchError(() => of([]))
-      );
+    return this._httpClient.get<Country[]>(url).pipe(catchError(() => of([])));
   }
 }
