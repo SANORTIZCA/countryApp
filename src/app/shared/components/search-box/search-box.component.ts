4f9bea28b40347c70e2c6071d80262ea2a5111ba
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-05-23 13:41:18 -0500

    Se implementa la limpieza de subscripciones, en este caso del debouncer, utlizando el ciclo de vida de componentes de angular onDestroy que implementa el método de ngOnDestroy cuando dejamos de utlizar el componente y nos ayuda a mantener la memoria de observables baja. EL único lugar donde no se implementa el ngOnDestroy es en las request http.get/post/patch ...

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``,
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy{
   /* Un subject es un tipo especial de observable, al cual se puede subscribir, hacer pipes */
   private _debouncer: Subject<string> = new Subject<string>();
+  /* Opcional porque en un punto del tiempo no tiene ninguna subscripción,
+  Es decir cuando se monta el componente */
+  private _debouncerSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -22,7 +25,7 @@ export class SearchBoxComponent implements OnInit {
   /* Siempre va despues del constructor */
   public ngOnInit(): void {
     /* con el debounce time lo que se espera es que cuando el usuario deje de escribir valores por x segundos se ejecute el subscribe */
-    this._debouncer
+    this._debouncerSubscription = this._debouncer
       .pipe(
         /* debounceTime: espera un tiempo para emitir la siguiente misión, pide 2 objetos, y uno es obligatorio */
         debounceTime(300)
@@ -32,6 +35,16 @@ export class SearchBoxComponent implements OnInit {
       });
   }
 
+  /* onDestroy, es un método que se va a llamar cuando la instancia de este componente va a ser destruida */
+  /* Ayuda a mantener la memoria de los observables al minimo */
+  /* Además es una parte del ciclo de vida de componentes en @angular */
+  /* Cuando se cambia de ruta angular llama todos los ngOnDestroy para actualizar el componente */
+  public ngOnDestroy(): void {
+    /* Se manejara el unsubscribe de manera independiente */
+    /* Esto se hace cada vez que se haya implementado un subscribe en un ngOnInit o en cualquier parte del componente que este escuchando cambios
+    La única donde no se usa es cuando se hacen request http, lo que se usa de @angular/common porque ya viene una forma de cerrar esas subscripciones */
+    this._debouncerSubscription?.unsubscribe();
+  }
   public emitValue(value: string): void {
     this.onValue.emit(value);
   }
