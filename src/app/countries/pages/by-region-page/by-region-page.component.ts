25767e16f11ef5062fb9c80de3c4f5c52a474361
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-05-23 17:05:31 -0500

    Se optimiza la busqueda por región y se limita mediante un type a los continentes y luego por medio de un ngclass se coloca que tipo de selección tiene en ese momento el usuario

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
+/* Las interfaces/interface se pueden expandir los tipos/type no */
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,13 +13,17 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByRegionPageComponent {
   public _countries: Country[] = [];
+  public _regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania',];
   private _countryService: CountriesService;
+  /* ES opcional porque cuando se carga la región, no se tiene  opción */
+  public _selectedRegion?: Region;
 
   constructor(countryService: CountriesService) {
     this._countryService = countryService;
   }
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+    this._selectedRegion = region;
     this._countryService.searchRegion(region).subscribe((countries) => {
       this._countries = countries;
     });
